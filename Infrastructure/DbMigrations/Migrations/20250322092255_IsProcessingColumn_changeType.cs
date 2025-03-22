6b3325d9f617be23fb1959d87673ff0ec943c9b7
Michał Białończyk <wujko95@gmail.com>
2025-03-22 10:23:58 +0100

    Zmiana typu kolumny

---
+++ Infrastructure/DbMigrations/Migrations/20250322092255_IsProcessingColumn_changeType.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsProcessingColumn_changeType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_processing",
+                table: "notification_executions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_processing",
+                table: "notification_executions",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+    }
+}
