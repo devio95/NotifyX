7d25109df03435935f1fdcd2af7a046109cb52ad
Michał Białończyk <wujko95@gmail.com>
2025-06-03 17:05:15 +0200

    Usunięcie zbędnych projektów

--- Workers/NotificationCalculator/Program.cs
+++
@@ -1,8 +0,0 @@
-using NotifyCalculator;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-builder.Services.AddDI();
-
-var host = builder.Build();
-host.Run();
